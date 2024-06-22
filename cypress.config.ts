241c90fa9933ecbcc06c21a63b93be80b36b7964
testsquadco <muhammadshariq.64@gmail.com>
2024-06-22 14:20:07 +0800

    add a basic test to validate the title

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:8080",
+    viewportWidth: 1200,
+    viewportHeight: 900,
+    env: {
+      email: 'shariq@yopmail.com',
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/support/pageObjects/practice-page.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+class PracticePage {
+
+    get title() {
+        return cy.get('div.wrapper h1');
+    }
+
+}
+export default new PracticePage()
